4298e1d486666b4ed2b8b70565cc5b5f7e8b60af
Rizwan Ahmed <46168791+Rizwan8909@users.noreply.github.com>
2020-07-25 13:37:03 -0700

    Final Method and Final class

---
+++ OOP_PHP/final.php
@@ -0,0 +1,26 @@
+<?php
+    class Father {
+        function display(){
+            echo "I can be overriden because I'm a normal function <br>";
+        }
+
+        final function disp(){
+            echo "I can not be overriden because I'm a final function <br>";
+        }
+    }
+
+    // Final Class ka child nhe bn sakta
+    final class Son extends Father {
+
+        function display(){
+             echo "I am overrided <br>";
+        }
+    }
+
+    $father = new Father();
+    $father->display();
+    $father->disp();
+
+    $son = new Son();
+    $son->display();
+?>
